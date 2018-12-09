48a9620b35b81396406d4debe062dfc74322fa2e
Florent Clarret <florent.clarret@gmail.com>
2018-12-09 18:30:46 +0100

    feat(borg): allow parameter to specify

--- borg/borg-backup.bash
+++ borg/borg-backup.bash
@@ -2,6 +2,18 @@
 
 source ../utils/logger.bash;
 
+usage() {
+  echo "./borg-backup.bash: "
+  echo -e "\tBackup folders using borg reading a configuration file"
+  echo
+  echo "Parameters: "
+  echo -e "\t-c|--configuration : specify the configuration file"
+  echo -e "\t-h|--help : Show this usage"
+  echo
+  echo "Configuration file format : "
+  echo -e "\tbackupName,sourceFolder,destinationFolder"
+}
+
 init_repo() {
     info "Initializing ${destination_folder} as a borg repo";
     borg init --encryption=none ${destination_folder} > /dev/null 2>&1
@@ -47,7 +59,25 @@ prune() {
     info "${backup_name} - End pruning"
 }
 
-CONFIGURATION_FILE="./configuration-file.example"
+CONFIGURATION_FILE="~/.config/borg-backup.conf"
+
+POSITIONAL=();
+while [[ $# -gt 0 ]]; do
+    case ${1} in
+        -c|--configuration)
+            CONFIGURATION_FILE=${2}; shift; shift;;
+        -h|--help)
+            usage; exit 0;;
+        *)
+            usage; exit 1;;
+    esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [[ ! -f ${CONFIGURATION_FILE} ]]; then
+    error "${CONFIGURATION_FILE} no such file";
+    exit 1;
+fi
 
 grep -v '^$\|^\s*\#' ${CONFIGURATION_FILE} | while read current_line; do
     backup_name=$(echo ${current_line} | cut -f1 -d',');
