c45d2d69961317da7f483bd7645ad7c8eb787766
Florent Clarret <florent.clarret@gmail.com>
2020-11-10 19:02:15 +0100

    feat(helios): add a script to watch the Helios UPS level

---
+++ utils/helios_watch_ups.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage() {
+  echo "./helios_watch_ups.bash: "
+  echo -e "\tWatch the UPS status and stop the host if needed"
+  echo
+  echo "Parameters: "
+  echo -e "\t-l|--level : (optionnal) The level from which you want to stop the ost"
+  echo -e "\t-h|--help : (optionnal) Show this usage"
+  echo
+}
+
+# ====== Main ======
+
+MIN_BATTERY_LEVEL="916"
+
+POSITIONAL=();
+while [[ $# -gt 0 ]]; do
+    case ${1} in
+        -l|--level)
+            MIN_BATTERY_LEVEL=${2}; shift; shift;;
+        -h|--help)
+            usage; exit 0;;
+        *)
+            usage; exit 1;;
+    esac
+done
+set -- "${POSITIONAL[@]}"
+
+ON_BATTERY=$(cat /sys/class/power_supply/gpio-charger/online)
+
+if [[ ${ON_BATTERY} -eq "1" ]]; then
+    CURRENT_LEVEL=$(scale=0;echo "`cat /sys/bus/iio/devices/iio:device0/in_voltage2_raw` * `cat /sys/bus/iio/devices/iio:device0/in_voltage_scale`/1" | bc)
+    if [[ "${CURRENT_LEVEL}" -lt ${MIN_BATTERY_LEVEL} ]]; then
+        poweroff
+    fi
+fi
+
+exit 0
